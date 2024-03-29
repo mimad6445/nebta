327eabead9617cdd3ea01e2e2257c6ac2b9e64d2
mimad6445 <mimad6445@gmail.com>
2024-03-29 04:57:04 +0100

    App Error added in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Connectdb = require('./database/Connectdb')
 const bodyParser = require('body-parser')
+const httpStatusText = require('./utils/httpStatusText');
 const cors = require('cors')
 require('dotenv').config()
 
@@ -29,6 +30,13 @@ app.use('/api/offredePromotion',offrePromotionRouter)
 
 const port = process.env.PORT || 8000
 
+app.all('*',(req,res,next)=>{
+    res.json({status : httpStatusText.ERROR , msg : "cannot found data"});
+})
+
+app.use('*',(error,req,res,next)=>{
+    res.status(error.statusCode || 500).json({status : httpStatusText.ERROR , msg : error.message});
+})
 
 app.listen(port,()=>{
     console.log(`Listening to ${port}`)
