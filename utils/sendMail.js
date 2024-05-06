fd905d2a000e9f1d4b36814d924e42c3597dd312
mimad6445 <mimad6445@gmail.com>
2024-05-06 18:45:09 +0100

    mail send

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -19,7 +19,24 @@ const sendMail = async (otp, email)=>{
         from: process.env.MAIL ,
         to: email,
         subject: 'nebtha Otp', 
-        text: `your nebtha verification code is ${otp}`
+        html: `<center>
+        <div style="font-family: Arial, sans-serif; padding: 20px; background-color: #eee;">
+            <div style="font-family: Arial, sans-serif; width:500px; heigth:500px background-color: #f3f4f6;">
+                <h1 style="color: #333;">nebtha Verification Code</h1>
+                <img src="cid:nebtha" style="max-width: 100%; height: auto;">
+                <p style="font-size: 16px;">Your nebtha verification code is <strong>${otp}</strong>.</p>
+            </div>
+        </div>
+    </center>
+    
+        `,
+        attachments: [
+            {
+                filename: 'nbtha.png',
+                path: '../../nebtha.png',
+                cid: 'nebtha'
+            }
+        ]
     };
     transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
